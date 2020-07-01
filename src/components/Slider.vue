24927b5316598ee8a7fd0a83f91d4a135833b80c
new4761 <brid47@outlook.com>
2020-07-01 23:15:41 +0700

    add sub slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,9 +1,10 @@
 <template>
+  <div>
   <v-layout row wrap>
-    <v-flex xs8 offset-xs1>
+
+    <v-flex xs8 offset-xs1 style="  z-index: 1;">
       <v-subheader class="pl-0 headtext">{{list.label}}</v-subheader>
       <v-slider
- 
        class="test"
         @change="givevalue(index,list.val)"
         v-model="list.val"
@@ -19,7 +20,21 @@
     <v-flex xs2>
       <img :src="require(`@/assets/png/${callimg(list.val)}`)" />
     </v-flex>
+             <v-flex xs8 offset-xs1 style="  z-index: 0;">
+      <v-slider
+      class="subSlider"
+      disabled
+        :tick-labels="ticksLabels"
+        :max="4"
+        step="1"
+        style="margin-top:-50px"
+        ticks="always"
+        tick-size="5"
+      ></v-slider>
+     </v-flex>
   </v-layout>
+
+  </div>
 </template>
 
 <script>
@@ -43,7 +58,13 @@ export default {
         "003-happy-1.png",
         "002-happy.png",
         "005-grinning.png"
-      ]
+      ],
+           ticksLabels: [
+          '1',
+          '2',
+          '3',
+          '4','5'
+        ],
     };
   },
   methods: {
@@ -69,6 +90,18 @@ export default {
 .v-slider__thumb {
   background-color: #1976d2 !important;
     border-color: #1976d2 !important;
+}
+
+.subSlider .v-slider__thumb {
+  background-color: #050505 !important;
+    border-color: #000000 !important;
+opacity: 0.3;
+}
+.subSlider.v-input--slider.v-input--slider--ticks .v-slider__ticks.v-slider__ticks--always-show {
+
+    background-color: #050505 !important;
+    border-color: #000000 !important;
+    opacity: 0.8;
 }
 
 </style>
